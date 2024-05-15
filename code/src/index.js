57c8bdc13e61b54af79c70a1dbfdd8119e42c6a0
Linh <linh.bk214910@sis.hust.edu.vn>
2024-05-15 21:17:56 +0700

    install prettier, lint-staged, husky

--- code/src/index.js
+++ code/src/index.js
@@ -1,37 +1,35 @@
-const path = require('path')
+const path = require('path');
 const morgan = require('morgan');
 const express = require('express');
-const { engine } = require ('express-handlebars');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3001;
 
 const route = require('./routes'); // đầy đủ là ./routes/index.js
 
 // add middleware để xử lí thông tin đươc gửi từ client
-app.use(express.urlencoded(
-  {extended:true}
-)); // data được gửi dưới dạng form
+app.use(express.urlencoded({ extended: true })); // data được gửi dưới dạng form
 app.use(express.json()); // data được gửi bằng file json?
 
-app.use(express.static(path.join(__dirname, 'public'))) 
+app.use(express.static(path.join(__dirname, 'public')));
 // nếu gặp đường dẫn kiểu ../img/image.jpg thì nó sẽ check thư mục public
 
 // HTTP logger
 // app.use(morgan('combined'));
 
 // Template engine
-app.engine('.hbs', engine({
-  extname: '.hbs'
-}));
+  app.engine(
+    '.hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-
-
-// console.log('PATH', path.join(__dirname, 'resources/views'));
 
 // routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-})
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
