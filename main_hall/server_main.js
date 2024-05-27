419e2186dba3bee1539f0b9c02e939428bea0e14
LongNT2003 <Long.NT214912@sis.hust.edu.vn>
2024-05-27 10:11:09 +0700

    update

--- main_hall/server_main.js
+++ main_hall/server_main.js
@@ -20,35 +20,42 @@ app.engine('.hbs', engine({
 app.set('view engine', 'hbs');
 
 
-
-const thu_muc_cau_hoi_s= [
-      {
-        avt: 'image.jpg',
-        name: 'Thư mục 1',
-        mon_hoc: 'Toán học',
-        number_of_question: 10
-      },
-      {
-        avt: 'image.jpg',
-        name: 'Thư mục 2',
-        mon_hoc: 'Vật lý',
-        number_of_question: 15
-      },
-      {
-        avt: 'image.jpg',
-        name: 'Thư mục 3',
-        mon_hoc: 'Hóa học',
-        number_of_question: 20
-      }
-    ];
-
-  
-
+let questions = [
+  {
+      title: "viết 1 đoạn văn miêu tả khung cảnh trường học vào mùa thu, viết 1 đoạn văn miêu tả khung cảnh trường học vào mùa thu",
+      content: "viết 1 đoạn văn miêu tả khung cảnh trường học vào mùa thu",
+      criteria: [
+        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp' và có 1 con chó biết nhảy,độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp' và có 1 con chó biết nhảy",
+        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
+        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
+      ]
+  },
+  {
+      title: "viết 1 đoạn văn",
+      content: "viết 1 đoạn văn miêu tả một buổi sáng tại công viên",
+      criteria: [
+        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
+        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
+        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
+      ]
+  }
+];
+for (let i=0;i<10;i++){
+    questions.push({
+        title: "viết 1 đoạn văn",
+        content: 'Cuộc sống riêng không biết đến điều gì xảy ra ngoài ngưỡng cửa nhà mình là một cuộc sống nghèo nàn, dù nó có đầy đủ tiện nghi đến đâu đi nữa. nó giống như một mảnh vườn được chăm sóc cẩn thận, đầy hoa thơm sạch sẽ và gọn gàng. Mảnh vườn này có thể làm chủ nhân của nó êm ấm một thời gian dài, nhất là khi lớp rào bao quanh không còn làm họ vướng mắt nữa. Nhưng hễ có một cơn dông tố nổi lên là cây cối sẽ bị bật khỏi đất, hoa sẽ nát và mảnh vườn sẽ xấu xí hơn bất kì một nơi hoang dại nào. Con người không thể hạnh phúc với một hạnh phúc mong manh như thế. Con người cần một đại dương mênh mông bị bão táp làm nổi sóng nhưng rồi lại phẳng lì và trong sáng như trước. Số phận cảu những cái tuyệt đối cá nhân không bộc lộ ra khỏi bản thân, chẳng có gì đáng thèm muốn.',     
+        criteria: [
+            "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
+            "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
+            "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
+          ]
+    })
+}
 
 app.get('/',(req,res) =>{
     res.render('home',{
-        style: 'app.css',
-        thu_muc_cau_hoi_s
+        style: 'home.css',
+        questions
 
     })
 })
