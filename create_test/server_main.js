a360e553f4ad35cb8e54308fc1c60d3ea39f819f
LongNT2003 <Long.NT214912@sis.hust.edu.vn>
2024-05-29 21:11:44 +0700

    create test

--- create_test/server_main.js
+++ create_test/server_main.js
@@ -20,28 +20,10 @@ app.engine('.hbs', engine({
 app.set('view engine', 'hbs');
 
 
-let questions = [
-  {
-      title: "viết 1 đoạn văn miêu tả khung cảnh trường học vào mùa thu, viết 1 đoạn văn miêu tả khung cảnh trường học vào mùa thu",
-      content: "viết 1 đoạn văn miêu tả khung cảnh trường học vào mùa thu",
-      criteria: [
-        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp' và có 1 con chó biết nhảy,độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp' và có 1 con chó biết nhảy",
-        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
-        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
-      ]
-  },
-  {
-      title: "viết 1 đoạn văn",
-      content: "viết 1 đoạn văn miêu tả một buổi sáng tại công viên",
-      criteria: [
-        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
-        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
-        "độ dài >200, bao gồm cụm từ 'buổi sáng tươi đẹp'",
-      ]
-  }
-];
-for (let i=0;i<10;i++){
+let questions = [];
+for (let i=0;i<2;i++){
     questions.push({
+        id:1,
         title: "viết 1 đoạn văn",
         content: 'Cuộc sống riêng không biết đến điều gì xảy ra ngoài ngưỡng cửa nhà mình là một cuộc sống nghèo nàn, dù nó có đầy đủ tiện nghi đến đâu đi nữa. nó giống như một mảnh vườn được chăm sóc cẩn thận, đầy hoa thơm sạch sẽ và gọn gàng. Mảnh vườn này có thể làm chủ nhân của nó êm ấm một thời gian dài, nhất là khi lớp rào bao quanh không còn làm họ vướng mắt nữa. Nhưng hễ có một cơn dông tố nổi lên là cây cối sẽ bị bật khỏi đất, hoa sẽ nát và mảnh vườn sẽ xấu xí hơn bất kì một nơi hoang dại nào. Con người không thể hạnh phúc với một hạnh phúc mong manh như thế. Con người cần một đại dương mênh mông bị bão táp làm nổi sóng nhưng rồi lại phẳng lì và trong sáng như trước. Số phận cảu những cái tuyệt đối cá nhân không bộc lộ ra khỏi bản thân, chẳng có gì đáng thèm muốn.',     
         criteria: [
@@ -58,10 +40,6 @@ app.get('/',(req,res) =>{
         questions
 
     })
-})
-
-app.get('/search',(req,res)=>{
-    res.render('search')
 })
 
 app.listen(8080,() =>{
