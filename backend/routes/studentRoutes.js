f99cbd1d5e99a7630fac959043af7451dd23e7cf
Lavibuu La <121961129+lavibula@users.noreply.github.com>
2024-06-18 01:46:20 +0700

    commit comment in route

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -6,9 +6,9 @@ const Enrollment = require('../models/Enrollment');
 
 const router = express.Router();
 
-router.post('/students/enroll', authMiddleware, studentController.enrollStudent);
-router.post('/students/essays', authMiddleware, studentController.submitEssay);
-router.get('/students/exams/:id/results', authMiddleware, studentController.getExamResults);
-router.get('/students/exams', authMiddleware, studentController.getAllExams);
-router.get('/students/exams/unsubmitted', authMiddleware, studentController.getUnsubmittedExams); 
+router.post('/students/enroll', authMiddleware, studentController.enrollStudent); //hàm dùng để tham gia Exam (truyền body examId)
+router.post('/students/essays', authMiddleware, studentController.submitEssay); // hàm dùng để submit bài thi (truyền body examquestionId, essayContent)
+router.get('/students/exams/:id/results', authMiddleware, studentController.getExamResults); //hàm dùng get result by exam (truyền param là examId)
+router.get('/students/exams', authMiddleware, studentController.getAllExams); // get all exams that student join
+router.get('/students/exams/unsubmitted', authMiddleware, studentController.getUnsubmittedExams); // get all exam that student doesn't submissions any questions
 module.exports = router;
